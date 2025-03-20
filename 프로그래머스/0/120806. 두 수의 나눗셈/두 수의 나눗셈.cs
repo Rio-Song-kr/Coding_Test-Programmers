267e389f84f9bfff2a30c2ac4ed9972f70704d77
Rio_Song <rio.dw.song@gmail.com>
2025-03-20 23:08:53 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
@@ -2,6 +2,6 @@ using System;
 
 public class Solution {
     public int solution(int num1, int num2) {
-        return (int)((double)num1 / num2 * 1000);
+        return (num1 * 1000) / num2;
     }
 }
